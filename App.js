9011fa36798ce5d7090a388098f54b4a8c134897
usman133 <usmankiani26@gmail.com>
2022-06-20 19:42:24 +1200

    Creating Sign in UI

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
 import LoginScreen from './Screens/LoginScreen';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +12,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen options={{HeaderShown: false}} name="Sign in" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,16 +1,99 @@
+import { CurrentRenderContext } from '@react-navigation/native'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
 
 const LoginScreen = () => {
 {
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
-  )
+    <KeyboardAvoidingView
+    style={styles.container}
+    behavior="padding">
+
+      <View style={styles.inputContainer}>
+        <TextInput
+        placeholder="Email"
+        // value={ }
+        // onChangeText={text => }
+        style={styles.input}
+        />
+         <TextInput
+        placeholder="Password"
+        // value={ }
+        // onChangeText={text => }
+        style={styles.input}
+        secureTextEntry
+        /> 
+      
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+        onPress={() => { }}
+        style={styles.button}
+        >
+          <Text style={styles.button}>Sign in</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+        onPress={() => { }}
+        style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+      
+    </KeyboardAvoidingView>
+)}
 }
-}
+
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+
+inputContainer: {
+  width: '80%'
+},
+input: {
+  backgroundColor: 'white', 
+  paddingHorizontal: 15,
+  paddingVertical: 10,
+  borderRadius: 5,
+  marginTop: 5,
+},
+buttonContainer: {
+  width:'60%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: 40,
+},
+button: {
+  backgroundColor: '#2da44e',
+  width: '100%',
+  padding: 15,
+  borderRadius: 10,
+  alignItems: 'center'
+},
+buttonOutline: {
+  backgroundColor: 'white',
+  marginTop: 5,
+  borderColor: '100%',
+  borderWidth: 2,
+},
+buttonText: {
+  color: 'white',
+  fontWeight: '700',
+  fontSize: 16,
+},
+buttonOutlineText: {
+  color: '#2da44e',
+  fontWeight: '700',
+  fontSize: 16,
+},
+})
